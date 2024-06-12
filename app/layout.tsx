157def89a88935866e7c5e3a8edc9104a570097f
hirshad494 <hamzairshad494@gmail.com>
2024-06-12 13:01:47 -0400

    Improved for analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
